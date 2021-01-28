f39741ca848d44e31ccac7c9ff44e60b2e298774
otaviopetry <otaviopetry@gmail.com>
2021-01-27 23:18:33 -0300

    chore: Initial commit
    Basic app structure

---
+++ src/pages/Dashboard/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from './styles';
+
+function Dashboard () {
+  return (
+    <Container>
+      <h1>Dashboard</h1>
+    </Container>
+  );
+}
+
+export default Dashboard;
---
+++ src/styles/globals.js
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const COLORS = {
+  mainPurple: '#3c3382',
+  brightPurple: '#7498cf',
+  mediumPuple: '#182365',
+  darkPurple: '#0b1435',
+  pink: '#aa68bf',
+};
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font: 1rem Ubuntu, sans-serif;
+    color: #333;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
