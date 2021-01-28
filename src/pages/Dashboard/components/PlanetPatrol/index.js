199fdab4823ad954a7ada66f462c843efc6ba792
otaviopetry <otaviopetry@gmail.com>
2021-01-28 13:38:08 -0300

    chore: Start implementing leaflet with crs simple
    And bounds

--- src/pages/Dashboard/components/PlanetPatrol/index.js
+++ src/pages/Dashboard/components/PlanetPatrol/index.js
@@ -5,8 +5,9 @@ import L from 'leaflet';
 import { MapContainer, TileLayer, GeoJSON, Popup } from 'react-leaflet';
 import { StarInfo } from './styles';
 
-import backgroundImage from '../../../../assets/images/nasa-galaxy.jpeg';
+import mapImage from '../../../../assets/images/nasa-galaxy.jpeg';
 import geoData from '../../../../assets/planet-patrol.json';
+
 
 function PlanetPatrol () {
   let tmagArray = [];
@@ -61,38 +62,12 @@ function PlanetPatrol () {
     );
   }
 
+  var bounds = [[0,0], [1000,1000]];
+
   return (
     <>
-    <MapContainer center={[10, 100]} zoom={5} className="map-container">
-      <TileLayer url={backgroundImage} />
-
-      {
-        geoData['features'].map((planet) => {
-            return ( 
-                <GeoJSON                         
-                    key={planet.properties.ID} 
-                    data={planet} 
-                    style={planet.geometry.type} 
-                    pointToLayer={() => myPointToLayer([planet.geometry.coordinates[1], planet.geometry.coordinates[0]], planet.properties.Tmag )}
-                    
-                >
-                    <Popup position={[0, -20]} onOpen={() => openStar(planet.properties.ID)} onClose={closeStar}>
-                        <strong>ID: </strong>{planet.properties.ID}<br />
-                        <strong>Tmag: </strong>{planet.properties.Tmag}<br />
-                        <strong>Kmag: </strong>{planet.properties.Kmag}<br />
-                        <strong>Vmag: </strong>{planet.properties.Vmag}<br />
-                        <strong>e_rad: </strong>{planet.properties.e_rad}<br />
-                        <strong>Mass: </strong>{planet.properties.mass}<br />
-                        <strong>pmDEC: </strong>{planet.properties.pmDEC}<br />
-                        <strong>pmRA: </strong>{planet.properties.pmRA}<br />
-                        <strong>ra: </strong>{planet.properties.ra}<br />
-                        <strong>rad: </strong>{planet.properties.rad}<br />
-                    </Popup>
-                </GeoJSON>                
-            );
-        })
-      }
-      
+    <MapContainer center={[10, 100]} zoom={5} className="map-container" crs="Simple">
+      <TileLayer url={mapImage} bounds={bounds} />
     </MapContainer>
     <StarInfo>
         <div id="currentStar">
