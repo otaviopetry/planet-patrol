0b9cdd79195792df2398bbcb652d78fd98622413
otaviopetry <otaviopetry@gmail.com>
2021-02-03 18:57:47 -0300

    chore: Restructure components
    Bring back Header and Planet Patrol to Dashboard index.js

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,13 +1,110 @@
-import React from 'react';
+import React, { useState } from 'react';
+import 'leaflet/dist/leaflet.css';
+import './styles.css';
+import L, { CRS } from 'leaflet';
+import { MapContainer, ImageOverlay, GeoJSON, Popup } from 'react-leaflet';
 
-import Header from '../../components/Header';
-import PlanetPatrol from './components/PlanetPatrol';
+import { Header, HeaderWrapper, HeaderTitle, SearchField, MainContainer, Sidebar } from './styles';
+
+import Panel from '../../components/Panel';
+
+import mapImage from '../../assets/images/universe-grid.png';
+import geoData from '../../assets/planet-patrol.json';
 
 function Dashboard () {
+  let tmagArray = [];
+  let kmagArray = [];
+
+  geoData.features.forEach(feature => {
+      tmagArray.push(feature.properties.Tmag);
+      kmagArray.push(feature.properties.Kmag);
+  })
+  
+  const [currentStar, setCurrentStar] = useState('');
+
+  function openStar (star) {
+      setCurrentStar(star);
+  }
+
+  function closeStar () {
+      setCurrentStar('');
+  }
+
+  const geojsonMarkerOptions = (radius) => {
+      return {
+          radius: radius/2,
+          fillColor: "#eaeaea",
+          color: "#fff",
+          weight: 2,
+          opacity: 1,
+          fillOpacity: 0.8
+      }
+  }
+
+  function myPointToLayer (latlng, radius) {
+      return L.circleMarker(latlng, geojsonMarkerOptions(radius));
+  }
+
+  function starInfo (star) {
+    const href = 'https://exofop.ipac.caltech.edu/tess/target.php?id=' + star;
+    return (
+      <>
+        <p>ID: {star}</p>
+        <a href={href} target="_blank" rel="noreferrer">Check more information on ExoFOP</a>
+      </>
+    );
+  }
+
+  var bounds = [[-120,-20], [120, 380]];
+
   return (
     <>
-      <Header />
-      <PlanetPatrol />
+    <Header>
+      <HeaderWrapper>
+        <HeaderTitle>Planet Patrol</HeaderTitle>
+        <SearchField />
+      </HeaderWrapper>
+    </Header>
+    
+    <MainContainer>
+      <Sidebar>
+        <Panel title="Current star" info={currentStar !== '' ? starInfo(currentStar.properties.ID) : '-'} />
+        <Panel title="Star coordinates" info={currentStar !== '' ? currentStar.geometry.coordinates[1] + ', ' + currentStar.geometry.coordinates[0] : '-'} />
+        <Panel title="Star Tmag" info={currentStar !== '' ? currentStar.properties.Tmag : '-'} />
+        <Panel title="Star Kmag" info={currentStar !== '' ? currentStar.properties.Kmag : '-'} />
+      </Sidebar>
+
+      <MapContainer center={[0, 180]} minZoom={1} zoom={2} maxBounds={bounds} className="map-container" crs={CRS.Simple}>
+        <ImageOverlay url={mapImage} bounds={bounds} />
+
+        {
+          geoData['features'].map((planet) => {
+              return ( 
+                  <GeoJSON                         
+                      key={planet.properties.ID} 
+                      data={planet} 
+                      style={planet.geometry.type} 
+                      pointToLayer={() => myPointToLayer([planet.geometry.coordinates[1], planet.geometry.coordinates[0]], planet.properties.Tmag )}
+                      
+                  >
+                      <Popup position={[0, -20]} onOpen={() => openStar(planet)} onClose={closeStar}>
+                          <strong>ID: </strong>{planet.properties.ID}<br />
+                          <strong>Tmag: </strong>{planet.properties.Tmag}<br />
+                          <strong>Kmag: </strong>{planet.properties.Kmag}<br />
+                          <strong>Vmag: </strong>{planet.properties.Vmag}<br />
+                          <strong>e_rad: </strong>{planet.properties.e_rad}<br />
+                          <strong>Mass: </strong>{planet.properties.mass}<br />
+                          <strong>pmDEC: </strong>{planet.properties.pmDEC}<br />
+                          <strong>pmRA: </strong>{planet.properties.pmRA}<br />
+                          <strong>ra: </strong>{planet.properties.ra}<br />
+                          <strong>rad: </strong>{planet.properties.rad}<br />
+                      </Popup>
+                  </GeoJSON>                
+              );
+          })
+        }
+      </MapContainer>    
+    </MainContainer>
     </>
   );
 }
--- src/pages/Dashboard/styles.js
+++ src/pages/Dashboard/styles.js
@@ -1,10 +1,42 @@
 import styled from 'styled-components';
 import { COLORS } from '../../styles/globals';
 
-export const Wrapper = styled.div`
+export const Header = styled.div`
   width: 100%;
+  background-color: #f0f0f5;
+  padding: 1.25rem;
+`;
+export const HeaderWrapper = styled.div`
+  max-width: 1300px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const HeaderTitle = styled.h1`
+  font-weight: bolder;
+  font-size: 1.4rem;
+  color: ${COLORS.mainPurple};
+  text-align: center;
+`;
+
+export const SearchField = styled.input`
+  width: 15rem;
+  height: 32px;
+  background-color: #fff;
+  color: #333;
+  border-radius: 0.5rem;
+  font-size: 1.25rem;
+`;
+
+export const MainContainer = styled.div`
+  width: 100%;
+  max-width: 1300px;
+  margin: 0 auto;
   padding: 3rem 0;
   display: flex;
+  justify-content: space-around;
   align-items: center;
 `;
 
@@ -12,10 +44,4 @@ export const Sidebar = styled.aside`
   width: 20rem;
   background-color: ${COLORS.darkPurple};
   padding-top: 2rem;
-`;
-
-export const Main = styled.main`
-  flex: 0.8;
-  display: flex;
-  flex-direction: column;
 `;
